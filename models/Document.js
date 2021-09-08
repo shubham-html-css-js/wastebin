0b8419ecc70ed19870cf45fd8746dcd3781ab746
shubham-html-css-js <90316825+shubham-html-css-js@users.noreply.github.com>
2021-09-08 19:58:41 +0530

    First Commit

---
+++ models/Document.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose')
+const documentScheme=new mongoose.Schema({
+    value:{
+        type:String,
+        required:true
+    }
+})
+module.exports=mongoose.model("document",documentScheme);
